f21801002bfc47b941a47b58c20fe142edcb0897
Arkan <arkankhudiyev@gmail.com>
2023-01-29 22:12:51 +0400

    new task added

---
+++ 28.01.2023 hefte sonu task/28.01.2023 hefte sonu task/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Dynamic;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
+namespace _28._01._2023_hefte_sonu_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            #region Verilmiş ədədin rəqəmləri cəmini tapan proqram
+            //int n = 357;
+            //int sum = 0;
+            //int num = 0;
+
+            //while (n > 0)
+            //{
+            //    num = n % 10;
+            //    sum += num;
+            //    n = (n - num) / 10;
+            //}
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region Verilmiş ədədlər siyahısındaki 21-ə bölenən ədədlərin ədədi ortasını tapan proqram
+
+            //int[] numbers = { 42, 63, 78, 84, 103 };
+            //int sum = 0;
+            //int count = 0;
+            //int edediortalama = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 21 == 0)
+            //    {
+            //        sum += numbers[i];
+            //        count++;
+            //        edediortalama = sum / count;
+            //    }
+
+            //}
+            //Console.WriteLine(edediortalama);
+            #endregion
+
+            #region Mezun oldunuz,Mezun olmadiniz....
+
+            //Console.WriteLine("qiymet deyeri");
+            //var pointstr = Console.ReadLine();
+            //int point = Convert.ToInt32(pointstr);
+
+
+
+            //if (point > 64 && point <= 100)
+            //{
+            //    Console.WriteLine("mezun oldunuz");
+
+            //}
+            //else if (point >= 0 && point < 65)
+            //{
+            //    Console.WriteLine("mezun olmadiniz");
+            //}
+
+
+
+            //while (point < 0 || point > 100)
+            //{
+
+            //    Console.WriteLine("Qiymet deyerini yeniden daxil edin!");
+            //    pointstr = Console.ReadLine();
+            //    point = Convert.ToInt32(pointstr);
+
+            //    if (point > 64 && point <= 100)
+            //    {
+            //        Console.WriteLine("mezun Oldunuz!");
+
+            //    }
+
+            //    else if (point >= 0 && point < 65)
+            //    {
+            //        Console.WriteLine("Mezun Olmadiniz!");
+            //    }
+            //}
+            #endregion
+        }
+
+    }
+}
