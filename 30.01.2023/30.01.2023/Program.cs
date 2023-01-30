104c5e33e3def14b213c57b92af32935a98b8d8e
Arkan <arkankhudiyev@gmail.com>
2023-01-31 02:08:03 +0400

    New Task

---
+++ 30.01.2023/30.01.2023/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _30._01._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region prints the sum of two numbers to the console
+            //    ShowSum(24, 30);
+
+
+            //}
+            //static void ShowSum(int num1, int num2)
+            //{
+
+            //    var result = num1 + num2;
+            //    Console.WriteLine(result);
+            #endregion
+
+            #region returns the sum of two numbers
+            //    var result = Sum(45, 20);
+            //    Console.WriteLine(result);
+            //}
+
+
+            //static int Sum(int num1, int num2)
+            //{
+            //    var result = num1 + num2;
+            //    return result;
+            #endregion
+
+            #region Method that returns the reverse side of the given text
+            //    var reverse = Reverse("Airplane");
+
+            //    Console.WriteLine(reverse);
+
+
+            //}
+            //static string Reverse(string str)
+            //{
+            //    string newStr = "";
+
+            //    for (int i = str.Length - 1; i >= 0; i--)
+            //    {
+            //        newStr += str[i];
+            //    }
+            //    return newStr;
+            #endregion
+
+
+            #region A method that returns the sum of the digits of a given number
+
+            //    var digitsSum = SumOfDigits(361);
+
+            //    Console.WriteLine(digitsSum);
+
+            //}
+            //static int SumOfDigits(int N)
+            //{
+
+            //    int sum = 0;
+
+            //    while (N > 0)
+            //    {
+            //        sum += N % 10;
+            //        N /= 10;
+            //    }
+            //    return sum;
+            #endregion
+
+            #region A method that returns the sum of the numbers in a given list of numbers
+            //    int[] nums = { 21, 19, 14, 16 };
+            //    var sumOfArr = Sum(nums);
+            //    Console.WriteLine(sumOfArr);
+
+            //}
+            //static int Sum(int[] nums)
+            //{
+            //    int sum = 0;
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        sum += nums[i];
+            //    }
+            //    return sum;
+            #endregion
+
+            #region  A method that writes to the console the sum of numbers from a given list of numbers
+            //    int[] numbers = { 5, 10, 8, 32, };
+            //    Sum(numbers);
+
+            //}
+
+            //static void Sum(int[] numbers)
+            //{
+            //    int sum = 0;
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        sum += numbers[i];
+            //    }
+            //    Console.WriteLine(sum);
+
+            #endregion
+
+            #region a method that returns the largest number in a given list of numbers
+
+            //    int[] nums = { 30, 50, 77, 91 };
+            //    var LargestNumber = Res(nums);
+            //    Console.WriteLine(LargestNumber);
+
+            //}
+
+            //static int Res(int[] nums)
+            //{
+            //    int maxnumber = nums[0];
+
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        if (nums[i] > maxnumber)
+            //        {
+            //            maxnumber = nums[i];
+            //        }
+
+            //    }
+            //    return maxnumber;
+            #endregion
+
+            #region A program that finds the largest of 3 given numbers
+
+        //    int Num1 = Convert.ToInt32(Console.ReadLine());
+        //    int Num2 = Convert.ToInt32(Console.ReadLine());
+        //    int Num3 = Convert.ToInt32(Console.ReadLine());
+
+        //    Numbers(Num1, Num2, Num3);
+        //}
+        //static void Numbers(int num1, int num2, int num3)
+
+        //{
+
+
+        //    if (num1 >= num2 && num1 >= num3)
+        //    {
+        //        Console.WriteLine(num1);
+
+        //    }
+        //    else if (num2 >= num1 && num2 >= num3)
+        //    {
+        //        Console.WriteLine(num2);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(num3);
+        //    }
+            #endregion
+
+
+
+
+        }
+
+
+    }
+
+
+}
