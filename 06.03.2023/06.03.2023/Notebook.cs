6e06851ae527e0b773b3f7bb62def4714bbbe6d7
Arkan <arkankhudiyev@gmail.com>
2023-02-07 00:25:57 +0400

    new

---
+++ 06.03.2023/06.03.2023/Notebook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace _06._03._2023
+{
+    internal class Notebook
+    {
+        public Notebook(string brand,string model) 
+        {
+            Brand = brand;
+            Model= model;
+        }
+        
+        public string Brand;
+        public string Model;
+        public byte Price;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} - Model: {Model} - Price: {Price}");
+        }
+    }
+}
---
+++ 06.03.2023/06.03.2023/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace _06._03._2023
+{
+
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Notebook ntb1 = new Notebook("MSI", "Sword 17")
+            {
+               
+                Price = 250,
+            };
+            ntb1.ShowInfo();
+            Notebook ntb2 = new Notebook("Acer", "Spin 3")
+            {
+               
+                Price = 150,
+            };
+            ntb2.ShowInfo();
+
+            Notebook[] notebooks = new Notebook[] { ntb1, ntb2 };
+            var sum = 0;
+            var result = 0;
+            var count = 0;
+            for (int i = 0; i < notebooks.Length; i++)
+            {
+                sum += notebooks[i].Price;
+                count++;
+
+            }
+            result = sum / count;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
